dc5ec4e060f61f215d6e59b524174cb0abda7833
doh-yo <doyo159090@gmail.com>
2024-07-11 01:32:15 +0900

    style: 헤더

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,40 +1,35 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import Logo from "@/images/logo/panda-market-logo.png";
-import profile from "@/images/logo/profile.svg";
-import styles from "./Header.module.css";
+import Logo from "@/../../public/images/logo/panda-market-logo.png";
+import profile from "@/../../public/images/logo/profile.svg";
 
 export default function Header() {
   const router = useRouter();
 
   return (
-    <header className={styles.globalHeader}>
-      <div className={styles.headerLeft}>
-        <Link href="/" className={styles.headerLogo}>
+    <header className="flex justify-between items-center w-full border-b border-gray10">
+      <div className="flex items-center ml-4">
+        <Link href="/" className="mr-4">
           <Image src={Logo} alt="판다마켓" width={153} height={50} />
         </Link>
         <nav>
-          <ul className={styles.globalHeaderNav}>
-            <li className={styles.navList}>
+          <ul className="flex gap-2">
+            <li className="px-[24px] py-[15px]">
               <Link href="/boards">
                 <span
-                  className={`${styles.navListLink} ${
-                    router.pathname === "/boards"
-                      ? styles.navListLinkActive
-                      : ""
-                  }`}
+                  className={
+                    router.pathname === "/boards" ? "text-primary" : ""
+                  }
                 >
                   자유게시판
                 </span>
               </Link>
             </li>
-            <li className={styles.navList}>
+            <li className="px-[24px] py-[15px]">
               <Link href="/items">
                 <span
-                  className={`${styles.navListLink} ${
-                    router.pathname === "/items" ? styles.navListLinkActive : ""
-                  }`}
+                  className={router.pathname === "/items" ? "text-primary" : ""}
                 >
                   중고마켓
                 </span>
@@ -43,10 +38,16 @@ export default function Header() {
           </ul>
         </nav>
       </div>
-
-      <Link href="/">
+      <div>
+        <Link href="/login">
+          <span className="border border-primary bg-primary text-white px-[23px] py-[12px] rounded-[8px]">
+            로그인
+          </span>
+        </Link>
+      </div>
+      {/* <Link href="/">
         <Image src={profile} alt="프로필" width={40} height={40} />
-      </Link>
+      </Link> */}
     </header>
   );
 }
