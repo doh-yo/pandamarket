9a7e5c386e763c80ddbeb870cf19190604401612
doh-yo <doyo159090@gmail.com>
2024-07-11 14:59:53 +0900

    fix: 빌드 에러 수정

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,16 +1,8 @@
-module.exports = {
+/** @type {import('postcss-load-config').Config} */
+const config = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
-    "postcss-preset-env": {
-      stage: 1,
-      features: {
-        "focus-within-pseudo-class": false,
-      },
-    },
-    "postcss-import": {},
-    "postcss-nested": {},
-    "postcss-global-import": {},
-    "postcss-custom-properties": {},
   },
 };
+
+export default config;
